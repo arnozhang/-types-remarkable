b8977a65c11ec126922873a36f5a76a463d3b53c
牧秦 <muqin.zyf@alibaba-inc.com>
2018-01-08 20:18:23 +0800

    Add Remarkable declares.

--- index.d.ts
+++ index.d.ts
@@ -5,4 +5,62 @@
  * @date 2017/11/23
  */
 
+export interface Options {
+    [key: string]: any;
 
+    html?: boolean;
+    breaks?: boolean;
+    linkify?: boolean;
+    typographer?: boolean;
+    xhtmlOut?: boolean;
+
+    quotes?: string;
+    langPrefix?: string,
+    linkTarget?: string;
+    maxNesting?: number;
+    highlight?: (str: string, language: string) => string;
+}
+
+
+export interface Presets {
+    options?: Options;
+    components?: any;
+}
+
+
+export declare type Plugin = (instance: Remarkable, options: any) => void;
+
+
+export interface StateCore {
+
+    src: string;
+    env: any;
+    options: Options;
+    tokens: any[];
+    inlineMode: boolean;
+
+    inline: any;
+    block: any;
+    renderer: any;
+    typographer: any;
+}
+
+
+export class Remarkable {
+
+    constructor(preset: string | Options, options?: Options);
+
+    set(options: Options): void;
+
+    configure(presets?: Presets): void;
+
+    use(plugin: Plugin, options: any): Remarkable;
+
+    parse(str: string, env: any): any[];
+
+    render(str: string, env: any): string;
+
+    parseInline(str: string, env: any): any[];
+
+    renderInline(str: string, env: any): string;
+}
